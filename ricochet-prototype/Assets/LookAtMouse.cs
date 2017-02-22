8bbb7e16b4a026ba07174334e4a9bd423be8083b
jih0 <jiho87@gmail.com>
2017-02-22 22:46:23 +1100

    mouse look
    updated code to include mouse look, however the rotation only occurs
    when the mouse button is clicked; this is not desired. desired effect is
    player looks at the mouse pointer/reticle all the time.

---
+++ ricochet-prototype/Assets/LookAtMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, 1, Input.mousePosition.z);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        lookPos = lookPos - player.transform.position;
+        float angle = Mathf.Atan2(lookPos.z, lookPos.x) * Mathf.Rad2Deg;
+        player.transform.rotation = Quaternion.AngleAxis(angle, Vector3.down);
+    }
+}
