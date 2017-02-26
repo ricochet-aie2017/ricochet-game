68c68529464922054400b32f0dc6fea09edec064
jih0 <jiho87@gmail.com>
2017-02-27 00:30:01 +1100

    added simple enemy spawner and raycast hit
    added simple enemies and enemy spawner, and implemented a simple raycast
    hit that destroys enemies

---
+++ ricochet-prototype/Assets/EnemyActor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActor : MonoBehaviour {
+
+    public float speed;
+    private PlayerActor player;
+    //private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindObjectOfType<PlayerActor>();
+        //controller = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dirToPlayer;
+        dirToPlayer = player.transform.position - this.transform.position;
+        dirToPlayer.Normalize();
+
+        this.transform.position += dirToPlayer * speed * Time.deltaTime;
+        //controller.Move(((dirToPlayer) * speed) * Time.deltaTime);
+	}
+}
