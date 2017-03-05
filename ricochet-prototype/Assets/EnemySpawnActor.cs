f2d648aad0cad0e80388a75626aa85f1ec58d976
jih0 <jiho87@gmail.com>
2017-03-06 00:41:43 +1100

    3 enemy types; 3 enemy spawn locations
    created 3 different basic enemy types that spawn randomly from 3
    different spawn locations on the map. also added a navmeshagent to the
    enemies so they can simply navigate around the level without hitting any
    walls

--- ricochet-prototype/Assets/EnemySpawnActor.cs
+++ ricochet-prototype/Assets/EnemySpawnActor.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class EnemySpawnActor : MonoBehaviour {
 
-    public GameObject enemy_prefab;
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
     public float spawn_time; // seconds between spawns
-    public float spawn_radius; // distance from player to spawn
+    //public float spawn_radius; // distance from player to spawn
 
     private PlayerActor player;
     private float spawn_timer; // timer that counts and controls spawning
 
 	// Use this for initialization
 	void Start () {
-        spawn_timer = spawn_time;
+        spawn_timer = Random.Range(spawn_time, 20);
         player = GameObject.FindObjectOfType<PlayerActor>();
 	}
 	
@@ -25,17 +27,33 @@ public class EnemySpawnActor : MonoBehaviour {
         if(spawn_timer < 0)
         {
             // reset the timer
-            spawn_timer = spawn_time;
+            spawn_timer = Random.Range(spawn_time, 20);
 
             // pick a random angle in radians and set the spawn point
-            float spawn_angle = Random.Range(0, 2 * Mathf.PI);
-            Vector3 spawn_direction = new Vector3(Mathf.Sin(spawn_angle), 0, Mathf.Cos(spawn_angle));
-            spawn_direction *= spawn_radius;
+            //float spawn_angle = Random.Range(0, 2 * Mathf.PI);
+            //Vector3 spawn_direction = new Vector3(Mathf.Sin(spawn_angle), 0, Mathf.Cos(spawn_angle));
+            //spawn_direction *= spawn_radius;
 
-            Vector3 spawn_point = player.transform.position + spawn_direction;
+            //Vector3 spawn_point = player.transform.position + spawn_direction;
+
+            Vector3 spawn_point = this.transform.position;
+
+            float randNum = Random.Range(0, 4);
 
             // spawn the enemy at the disired location
-            Instantiate(enemy_prefab, spawn_point, Quaternion.identity);
+
+            if (randNum > 2)
+            {
+                Instantiate(enemy3, spawn_point, Quaternion.identity);
+            }
+            else if (randNum <= 2 && randNum > 1)
+            {
+                Instantiate(enemy2, spawn_point, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(enemy1, spawn_point, Quaternion.identity);
+            }
         }
 	}
 }
