646161f9accd335c4377e7662d5d1a6c9c1f8a19
Atomizer <shirlaw@internode.on.net>
2017-03-26 07:05:48 +1000

    New assets, damage mechanics and movement
    Added bullet damage
    Added bounce counter and bounce destruction
    Added bonus damage based on bounce count
    Changed health on enemies to int
    Added TakeDamage/Kill to Enemies
    Redid movement, moved movement into PlayerActor script
    Added models, sounds and textures from google drive
    Disabled capsule renderer on player
    Added wheelchair and pistol mesh to player
    Added basic sound playback to shooting

---
+++ ricochet-prototype/Assets/Scripts/EnemyActor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActor : MonoBehaviour {
+
+    public float speed;
+    public int health;
+    private PlayerActor player;
+    //private CharacterController controller;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindObjectOfType<PlayerActor>();
+        //controller = gameObject.GetComponent<CharacterController>();
+
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 dirToPlayer;
+        //dirToPlayer = player.transform.position - this.transform.position;
+        //dirToPlayer.Normalize();
+
+        //this.transform.position += dirToPlayer * speed * Time.deltaTime;
+        //controller.Move(((dirToPlayer) * speed) * Time.deltaTime);
+
+        agent.destination = player.transform.position;
+    }
+
+    // Handles damaging this enemy
+    public void TakeDamage(GameObject damager, int damage)
+    {
+        int dmg = damage;
+        OnDamage(damager, ref dmg);
+        
+        health -= dmg;
+
+        if (health < 0)
+            Kill(damager);
+    }
+
+    protected virtual void OnDamage(GameObject damager, ref int damage)
+    {
+        //TODO: Extra stuff to be done when taking damage, as an example, damage reduction
+        //damage -= 1;
+    }
+
+    /// <summary>
+    /// Handles destruction of this enemy
+    /// </summary>
+    /// <param name="killer"></param>
+    public void Kill(GameObject killer)
+    {
+        OnKilled(killer);
+
+        Destroy(this.gameObject);
+    }
+
+    protected virtual void OnKilled(GameObject killer)
+    {
+        //TODO: Any stuff to be done before being destroyed is done here
+    }
+}
---
+++ ricochet-prototype/Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : MonoBehaviour {
+    // TODO: Create a weapon system
+    public AudioClip ShootSound = null; // Sound to play when shooting
+	public GameObject Projectile = null; // The projectile to spawn
+	public GameObject Barrel = null; // The spot to spawn projectiles from
+    public float recoilFactor = 1.0f; // Amount of recoil, TODO: Maybe make a weapon class to put this code in instead?
+    public float TimeBetweenShots = 0.5f; // Actual time between shooting
+	private float shootTimer = 0.0f; // Countdown timer until the next shot can be fired
+
+    //public float speed = 10.0f;
+
+    //private CharacterController controller;
+    //private Vector3 mpos;
+    private Vector3 moveDirection; // Bringing this back to do more raw movement using the character controller
+    //private Vector3 start;
+    //private Vector3 end;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouse_pos = Input.mousePosition;
+        Ray mouse_ray = Camera.main.ScreenPointToRay(mouse_pos);
+        Plane player_plane = new Plane(Vector3.up, transform.position);
+
+        float ray_distance = 0;
+        player_plane.Raycast(mouse_ray, out ray_distance);
+
+        Vector3 cast_point = mouse_ray.GetPoint(ray_distance);
+
+        Vector3 to_cast_point = cast_point - transform.position;
+        to_cast_point.Normalize();
+        Ray fire_ray = new Ray(transform.position, to_cast_point);
+
+        /* Should no longer need this
+        RaycastHit info;
+        if (Input.GetMouseButton(0) && Physics.Raycast(fire_ray, out info))
+        {
+            if(info.collider.tag == "Enemy")
+            {
+                Destroy(info.collider.gameObject);
+            }
+        }*/
+        
+        // Movement
+        GetComponent<CharacterController>().Move((moveDirection * recoilFactor) * Time.deltaTime);
+        moveDirection *= 0.9f; // Falloff
+
+        // Update timer
+        if (shootTimer > 0)
+            shootTimer -= Time.deltaTime;
+    }
+
+	// Spawns and shoots a new bullet projectile
+	public void Shoot()
+	{
+		if (Projectile == null || Barrel == null)
+		{
+			Debug.LogWarning("Player attempting to shoot projectile, but projectile or barrel not set");
+			return;
+		}
+
+		// Prevent shooting before time
+		if (shootTimer > 0)
+			return;
+
+        // Play the audio shoot sound.
+        GetComponent<AudioSource>().PlayOneShot(ShootSound);
+
+		var proj = GameObject.Instantiate<GameObject>(Projectile);
+		proj.transform.position = Barrel.transform.position;
+		proj.transform.rotation = Barrel.transform.rotation;
+		proj.GetComponent<Rigidbody>().AddForce(Barrel.transform.forward * 1000);
+		shootTimer += TimeBetweenShots;
+
+        // Do the knockback movement here
+        moveDirection = -(Barrel.transform.forward.normalized * recoilFactor);
+        
+        /*RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.cyan);
+            Vector3 unit = (hit.point - this.transform.position).normalized;
+            Vector3 destination = this.transform.position + ((-unit) * recoilFactor);
+            // hit.point: +ve = move towards, -ve = move away
+            //agent.destination = destination;
+            GetComponent<CharacterController>().Move(((-unit) * recoilFactor));
+            //print(agent.destination);
+        }*/
+    }
+}
